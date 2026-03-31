49b77d43cfad98883a6b45f22a01d37ec414e490
232417 <232417@DESKTOP-1K0QRTR>
2026-03-31 15:05:46 +0300

    Добавьте файлы проекта.

---
+++ stasik/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stasik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            Car car2 = new Car("Audi","Q8", 450);
+            Car car3 = new Car("BMW", "X3", 300);
+
+            car1.Printinfo();
+            car2.Printinfo();
+            car3.Printinfo();
+            Console.WriteLine($"Общее количество машин: {Car.GetTotalCars()}");
+        }
+    }
+    class Car
+    {
+        public string Brand;
+        public string Model;
+        public int Speed;
+        private static int totalCarsCreated = 0;
+
+        public Car()
+        {
+            Brand = "Unknown";
+            Model = "Unknown";
+            Speed = 0;
+            totalCarsCreated++;
+        }
+        public Car(string brand, string model, int speed)
+        {
+            Brand = brand;
+            Model = model;
+            Speed = speed;
+            totalCarsCreated++;
+        }
+        public static int GetTotalCars()
+        {
+            return totalCarsCreated;
+        }
+        public void Printinfo()
+        {
+            Console.WriteLine($"Бренд авто: {Brand}. Модель: {Model}. Скорость: {Speed}.");
+        }
+    }
+}
---
+++ stasik/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("stasik")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("stasik")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("579d2f03-4b76-4bfc-9fe7-cbb03fdb27b4")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
