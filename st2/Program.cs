c09bbe5cf36e818dbfb5a6baba5037e35b4e5bd8
Азалия Салихова <svlihovaazalia@gmail.com>
2026-04-06 11:50:59 +0300

    lol

--- st2/Program.cs
+++ st2/Program.cs
@@ -10,11 +10,54 @@ namespace st2
     {
         static void Main(string[] args)
         {
+            Student student1 = new Student();
+            Student student2 = new Student("Sasha");
+            Student student3 = new Student("Roma", 19);
+            Student student4 = new Student("Gosha", 21, "SIS", 4.6);
 
+            student1.PrintInfo();
+            student2.PrintInfo();
+            student3.PrintInfo();
+            student4.PrintInfo();
+
+            Console.WriteLine(Student.GetTotalStudents());
         }
     }
     class Student
     {
-        public string 
+        public string Name;
+        public int Age;
+        public string Group;
+        public double AverageGrade;
+        private static int totalStudents = 0;
+        public Student()
+        {
+            Name = "Неизвестно";
+            Age = 18;
+            Group = "Не определена";
+            AverageGrade = 0.0;
+            totalStudents++;
+        }
+        public Student(string n) : this()
+        {
+            Name = n;
+        }
+        public Student(string n, int age) : this(n)
+        {
+            Age = age;
+        }
+        public Student(string n, int age, string g, double a) : this(n, age)
+        {
+            Group = g;
+            AverageGrade = a;
+        }
+        public static int GetTotalStudents()
+        {
+            return totalStudents;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Имя: {Name}, Возвраст: {Age}, Группа: {Group}, Средний балл: {AverageGrade:F2}.");
+        }
     }
 }
---
+++ st3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace st3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Введите курс доллара");
+            double usd = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Введите курс евро");
+            double euro = Convert.ToDouble(Console.ReadLine());
+            CorrencyConverter.SetRates(usd, euro);
+            Console.WriteLine($"Сколько долларов вы хотите обменять");
+            double usdConvert = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Вы получите {CorrencyConverter.ConvertUsdToRub(usdConvert)} руб");
+            Console.WriteLine($"Сколько евро вы хотите обменять");
+            double euroConvert = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Вы получите {CorrencyConverter.ConvertEuroToRub(euroConvert)} руб");
+        }
+    }
+    class CorrencyConverter
+    {
+        private static double UsdToRubRate;
+        private static double EuroToRubRate;
+        public static void SetRates(double usdRate, double euroRate)
+        {
+            UsdToRubRate = usdRate;
+            EuroToRubRate = euroRate;
+            Console.WriteLine($"Курсы валют установленны");
+        }
+        public static double ConvertUsdToRub(double usd)
+        {
+            return usd * UsdToRubRate;
+        }
+        public static double ConvertEuroToRub(double euro)
+        {
+            return euro * EuroToRubRate;
+        }
+    }
+}
